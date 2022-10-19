a42d4a69456e73ffc234acf2d8221b4f20b72db1
Aksaev <mail-ax@ya.ru>
2022-10-19 20:23:12 +0400

    Урок 2.1 ООП. Выполнил.

---
+++ src/Car.java
@@ -0,0 +1,63 @@
+public class Car {
+    String brand;
+    String model;
+    Integer productionYear;
+    String productionCountry;
+    String color;
+    Double engineVolume;
+
+    Car (String brand, String model, Integer productionYear, String productionCountry, String color, Double engineVolume) {
+
+        // Если не указана модель
+        if ((brand == null || brand.length() == 0)) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+
+        // Если не указана марка
+        if ((model == null || model.length() == 0)) {
+            this.model = "default";
+        } else {
+            this.model = model;
+        }
+
+        // Если не указана страна сборки
+        if ((productionCountry == null || productionCountry.length() == 0)) {
+            this.productionCountry = "default";
+        } else {
+            this.productionCountry = productionCountry;
+        }
+
+        // Если не указан объем двигателя
+        if (engineVolume == null) {
+            this.engineVolume = 1.5;
+        } else {
+            this.engineVolume = engineVolume;
+        }
+
+        // Если не указан цвет
+        if (color == null || color.length() == 0) {
+            this.color = "Белый";
+        } else {
+            this.color = color;
+        }
+
+        // Если не указан год
+        if (productionYear == null) {
+            this.productionYear = 2000;
+        } else {
+            this.productionYear = productionYear;
+        }
+    }
+
+    void printCar() {
+        System.out.println(brand + " " + model + ":\n" +
+                "  - Год выпуска: " + productionYear + "\n" +
+                "  - Сборка: " + productionCountry + "\n" +
+                "  - Цвет кузова: " + color + "\n" +
+                "  - Объем двигателя: " + engineVolume + " литра");
+
+        System.out.println("-----------------------------------------------------------");
+    }
+}
